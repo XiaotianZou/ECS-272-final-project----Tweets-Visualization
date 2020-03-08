8b0f64815573c4cb60ae2654297aca12192545b1
snsnlou <15103097d@connect.polyu.hk>
2020-03-07 17:16:01 -0800

    Added Stream Graph in Main View

--- scripts/bandGraph.js
+++ scripts/bandGraph.js
@@ -5,6 +5,8 @@ var bandGraphInnerHeight, bandGraphInnerWidth
 
 var bandGraphSVG = null
 
+var xScaleBandGraph, countScaleBandGraph, valanceScaleBandGraph
+
 function initBandGraph() {
     bandGraphInnerHeight = bandGraphHeight - bandGraphMargin.top - bandGraphMargin.bottom
     bandGraphInnerWidth = bandGraphWidth - bandGraphMargin.left - bandGraphMargin.right
@@ -13,13 +15,58 @@ function initBandGraph() {
         .attr('width', bandGraphWidth)
         .attr('height', bandGraphHeight)
         .append('g')
-        .attr('transform', 'translate（' + bandGraphMargin.left + ',' + bandGraphMargin.top + ')')
+        .attr('transform', 'translate(' + bandGraphMargin.left + ',' + bandGraphMargin.top + ')')
+
+    xScaleBandGraph = d3.scaleTime().range([0, bandGraphInnerWidth])
+    countScaleBandGraph = d3.scaleLinear().range([bandGraphInnerHeight, 0])
 }
+
+initBandGraph()
 
 function onChangeBandGraph() {
     bandGraphSVG.selectAll('*')
         .remove()
     
-    var allTweets = null
+    // console.log(allTweets)
+    var tweetCountByDate = d3.nest().key(function(d) {
+        var time = d['time']
+        return time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate()
+    }).rollup(function(leaves) {
+        return leaves.length
+    }).entries(allTweets)
+
+    tweetCountByDate.sort(function(a, b) {
+        return (Date.parse(a['key']) > Date.parse(b['key']))?1:-1
+    })
+    // console.log(tweetCountByDate)
+    xScaleBandGraph.domain(d3.extent(tweetCountByDate, function(d) {return Date.parse(d['key'])}))
+    countScaleBandGraph.domain([0, 2 * d3.max(tweetCountByDate, function(d) {return d['value']})])
+
+    var xAxisBandGraph = d3.axisBottom()
+        .scale(xScaleBandGraph)
+    var countAxisBandGraph = d3.axisRight()
+        .scale(countScaleBandGraph)
     
+    var stack = d3.stack().keys(['value'])
+    var layers = stack(tweetCountByDate)
+    // console.log(layers)
+    var area = d3.area()
+        .x(function(d) {return xScaleBandGraph(Date.parse(d.data['key']))})
+        .y0(function(d) {return countScaleBandGraph(d[0])})
+        .y1(function(d) {return countScaleBandGraph(d[1])})
+
+    bandGraphSVG.selectAll('path').remove()
+    bandGraphSVG.selectAll('path')
+        .data(layers)
+        .enter()
+        .append('path')
+        .style('fill', '#B5CCE1')// R 181 G 204 B 225
+        .attr('d', area)
+    
+    bandGraphSVG.append('g')
+        .call(xAxisBandGraph)
+        .attr('transform', 'translate(0, ' + bandGraphInnerHeight +')')
+    bandGraphSVG.append('g')
+        .call(countAxisBandGraph)
+        .attr('transform', 'translate(' + bandGraphInnerWidth + ', 0)')
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,26 @@
 var data = null
+var allTweets = null
 
 function getData() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            data = JSON.parse(this.responseText)
-            console.log(data)
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            data = JSON.parse(this.responseText).data
+            // console.log(data)
+
+            allTweets = []
+            data.forEach(function (d) {
+                for (var j = 0; j < d['time'].length; j++) {
+                    var o = {}
+                    o['text'] = d['words'][j]
+                    var time = new Date(d['time'][j])
+                    // o['time'] = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate()
+                    o['time'] = time
+                    allTweets.push(o)
+                }
+            })
+
+            onChangeBandGraph()
         }
     }
     xhttp.open('GET', 'http://127.0.0.1:8000/getinfo', true)
