86f86dfbb9ee57ef221287b14605ebef86514d0f
snsnlou <15103097d@connect.polyu.hk>
2020-03-08 22:09:03 -0700

    Added Tag Cloud

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,21 @@ var data = null
 var allTweets = null
 
 function getData() {
+    init()
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.responseText).data
-            
-            data.forEach(function(d) {
-                for(var j = 0; j < d['time'].length; j++) {
+
+            data.forEach(function (d) {
+                for (var j = 0; j < d['time'].length; j++) {
                     d['time'][j] = new Date(d['time'][j])
                 }
                 d['earlyTime'] = d3.min(d['time'])
             })
             // console.log(data)
-            data.sort(function(a, b) {
-                return (a['earlyTime'] > b['earlyTime'])? 1:-1
+            data.sort(function (a, b) {
+                return (a['earlyTime'] > b['earlyTime']) ? 1 : -1
             })
             allTweets = []
             data.forEach(function (d) {
@@ -34,4 +35,9 @@ function getData() {
     xhttp.open('GET', 'http://127.0.0.1:8000/getinfo', true)
     // xhttp.setRequestHeader('Content-Type', 'application/json')
     xhttp.send()
+}
+
+function init() {
+    initBandGraph()
+    initTagCloud()
 }
---
+++ scripts/tagCloud.js
@@ -0,0 +1,82 @@
+var tagCloudMargin = { top: 5, bottom: 5, left: 5, right: 5 }
+var tagCloudHeight = 300
+var tagCloudWidth = 300
+var tagCloudInnerHeight, tagCloudInnerWidth
+
+var tagCloudSVG = null
+var xScaleTagCloud = null
+
+function initTagCloud() {
+    tagCloudInnerHeight = tagCloudHeight - tagCloudMargin.top - tagCloudMargin.bottom
+    tagCloudInnerWidth = tagCloudWidth - tagCloudMargin.left - tagCloudMargin.right
+    d3.select('#tagCloudSVG').selectAll('*').remove()
+    tagCloudSVG = d3.select('#tagCloudSVG')
+        .attr('width', tagCloudWidth)
+        .attr('height', tagCloudHeight)
+        .append('g')
+        .attr('transform', 'translate(' + tagCloudWidth / 2 + ', ' + tagCloudHeight / 2 + ')')
+
+
+}
+
+function onChangeTagCloud() {
+    tagCloudSVG.selectAll('text').remove()
+    var allText = data[hoveredClusterIndex].words.join()
+    var wordCount = {}
+    var words = allText.split(/[ ,.!?]+/)
+    words.forEach(function (d) {
+        if (isValidWordTagCloud(d)) {
+            if (wordCount[d]) {
+                wordCount[d] += 1
+            } else {
+                wordCount[d] = 1
+            }
+        }
+    })
+    wordCount = d3.entries(wordCount)
+    // console.log(wordCount)
+    xScaleTagCloud = d3.scaleLinear().range([10, 95])
+        .domain(d3.extent(wordCount, function(d) {return d.value}))
+    
+    var layoutTagCloud = d3.layout.cloud()
+        .size([tagCloudInnerWidth, tagCloudInnerHeight])
+        .words(wordCount)
+        .fontSize(function(d) {
+            return xScaleTagCloud(d.value)
+        })
+        .rotate(0)
+        .font('Serif')
+        .on('end', drawTagCloud)
+        .start()
+    
+    d3.layout.cloud().stop()
+}
+
+function drawTagCloud(words) {
+    tagCloudSVG.selectAll('text')
+        .data(words)
+        .enter()
+        .append('text')
+        .style('font-size', function(d) {
+            return xScaleTagCloud(d.value) + 'px'
+        })
+        .style('font-family', 'Serif')
+        .attr('text-anchor', 'middle')
+        .attr('transform', function(d) {
+            // console.log('(' + d.x + ', ' + d.y + ')')
+            return 'translate(' + d.x + ', ' + d.y +')'
+        })
+        .text(function(d) {
+            return d.key
+        })
+}
+
+// Used to remove invalid words from the tag cloud
+function isValidWordTagCloud(string) {
+    if(string.length < 1) return false
+    if(string.includes('#')) return false
+    if(string.includes('@')) return false
+    if(string.includes('http://')) return false
+    if(string.includes('https://')) return false
+    return true
+}
