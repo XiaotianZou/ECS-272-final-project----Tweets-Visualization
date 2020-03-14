ec2341a35a2a6acb9752cbd275591da75c4c5806
shuotwang <shuotongwang@gmail.com>
2020-03-13 23:15:10 -0700

    time window finished (color to be changed) pos1 and pos2 are two position

--- scripts/timeWindow.js
+++ scripts/timeWindow.js
@@ -91,10 +91,40 @@ s1.on('mouseup', function (e) {
     r2.visible(true);
     r2.listening(true);
     s1.draw();
+
+    if (isResizingLeft){
+        isResizingLeft = false;
+    }
+
+    if (isResizingRight){
+        isResizingRight = false;
+    }
+
 })
 
 r1.on('mouseover', function(e) {
-    
+    r2.draggable(true);
+})
+
+s1.on('mousemove', function(e){
+    if (isResizingLeft){
+        document.body.style.cursor = 'ew-resize';
+
+        pos1 = {x: e.evt.layerX, y: canvasPos.y}
+        leftX = pos1.x < pos2.x ? pos1.x : pos2.x;
+        r2.x(leftX);
+        r2.width(Math.abs(pos1.x - pos2.x));
+        s1.draw()
+
+    } else if (isResizingRight){
+        document.body.style.cursor = 'ew-resize';
+
+        pos2 = {x: e.evt.layerX, y: canvasPos.y}
+        leftX = pos1.x < pos2.x ? pos1.x : pos2.x;
+        r2.x(leftX);
+        r2.width(Math.abs(pos1.x - pos2.x));
+        s1.draw()
+    }
 })
 
 
@@ -104,26 +134,42 @@ r2.on('mouseout', function() {
 });
 
 r2.on('mousemove', function(e){
+
     if (Math.abs(e.evt.layerX - r2.x()) < 3 || Math.abs(e.evt.layerX - r2.x() - r2.width()) < 3){
         document.body.style.cursor = 'ew-resize';
-
     }else if (e.evt.layerX > r2.x() || e.evt.layerX < r2.x() + r2.width()){
         document.body.style.cursor = 'move';
     }
+    
     r1.listening(false);
 })
 
+
+var pos1, pos2;
+var isResizingLeft = false, isResizingRight = false;
+
 r2.on('mousedown', function(e) {
     if(Math.abs(e.evt.layerX - r2.x()) < 3){
-        s1.on('mousemove', function(e){
-            console.log('something')
-        })
+        console.log("r2 mouse down left")
 
+        pos1 = {x: r2.x(), y: canvasPos.y}
+        pos2 = {x: r2.x() + r2.width(), y: canvasPos.y}
+
+        isResizingLeft = true
+        r2.draggable(false);
         
     }else if (Math.abs(e.evt.layerX - r2.x() - r2.width()) < 3){
+
+        pos1 = {x: r2.x(), y: canvasPos.y}
+        pos2 = {x: r2.x() + r2.width(), y: canvasPos.y}
+
         // right edge
+        console.log("r2 mouse down right")
+        isResizingRight = true
+        r2.draggable(false);
     }
 })
+
 
 
 // reverse co-ords if user drags left / up
@@ -139,68 +185,3 @@ function reverse(r1, r2) {
     }
     return ({ x1: r1x, y1: r1y, x2: r2x, y2: r2y }); // return the corrected rect.     
 }
-
-
-// following: line chart
-// var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-//     , width = window.innerWidth - margin.left - margin.right // Use the window's width 
-//     , height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
-
-//     // The number of datapoints
-// var n = 21;
-
-// // 5. X scale will use the index of our data
-// var xScale = d3.scaleLinear()
-//     .domain([0, n - 1]) // input
-//     .range([0, width]); // output
-
-// // 6. Y scale will use the randomly generate number 
-// var yScale = d3.scaleLinear()
-//     .domain([0, 1]) // input 
-//     .range([height, 0]); // output 
-
-// // 7. d3's line generator
-// var line = d3.line()
-//     .x(function (d, i) { return xScale(i); }) // set the x values for the line generator
-//     .y(function (d) { return yScale(d.y); }) // set the y values for the line generator 
-//     .curve(d3.curveMonotoneX) // apply smoothing to the line
-
-// // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-// var dataset = d3.range(n).map(function (d) { return { "y": d3.randomUniform(1)() } })
-
-// // 1. Add the SVG to the page and employ #2
-// var svg = d3.select("body").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//     .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// // 3. Call the x axis in a group tag
-// svg.append("g")
-//     .attr("class", "x axis")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
-
-// // 4. Call the y axis in a group tag
-// svg.append("g")
-//     .attr("class", "y axis")
-//     .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
-
-// // 9. Append the path, bind the data, and call the line generator 
-// svg.append("path")
-//     .datum(dataset) // 10. Binds data to the line 
-//     .attr("class", "line") // Assign a class for styling 
-//     .attr("d", line); // 11. Calls the line generator 
-
-// // 12. Appends a circle for each datapoint 
-// svg.selectAll(".dot")
-//     .data(dataset)
-//     .enter().append("circle") // Uses the enter().append() method
-//     .attr("class", "dot") // Assign a class for styling
-//     .attr("cx", function (d, i) { return xScale(i) })
-//     .attr("cy", function (d) { return yScale(d.y) })
-//     .attr("r", 5)
-//     .on("mouseover", function (a, b, c) {
-//         console.log(a)
-//     })
-//     .on("mouseout", function () { })
